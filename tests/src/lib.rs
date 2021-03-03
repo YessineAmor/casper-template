22daef945a2a8c8c50d2accbfce30ea2a57dc70b
YessineAmor <yassine.amor@esprit.tn>
2021-03-03 11:48:15 +0100

    update tests

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -6,25 +6,25 @@ mod tests {
 
     #[test]
     fn should_initialize_to_zero() {
-    let ali : PublicKey = PublicKey::ed25519([3u8; 32]).unwrap();
-    let MY_ACCOUNT: AccountHash = ali.to_account_hash();
+    let my_account : PublicKey = PublicKey::ed25519([3u8; 32]).unwrap();
+    let my_account_hash: AccountHash = my_account.to_account_hash();
     const KEY: &str = "special_value";
     const CONTRACT: &str = "tutorial";
 
         let mut context = TestContextBuilder::new()
-            .with_public_key(ali,ali.to_account_hash(), U512::from(500_000_000_000_000_000u64))
+            .with_public_key(my_account,my_account_hash, U512::from(500_000_000_000_000_000u64))
             .build();
         let session_code = Code::from("contract.wasm");
         let session_args = runtime_args! {
             "initial_value" => 0u64
         };
         let session = SessionBuilder::new(session_code, session_args)
-            .with_address(MY_ACCOUNT)
-            .with_authorization_keys(&[MY_ACCOUNT])
+            .with_address(my_account_hash)
+            .with_authorization_keys(&[my_account_hash])
             .with_block_time(0)
             .build();
         context.run(session);
-        let check: u64 = match context.query(MY_ACCOUNT, &[CONTRACT.to_string(), KEY.to_string()]) {
+        let check: u64 = match context.query(my_account_hash, &[CONTRACT.to_string(), KEY.to_string()]) {
             Err(_) => panic!("Error"),
             Ok(maybe_value) => maybe_value
                 .into_t()
